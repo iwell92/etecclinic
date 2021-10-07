464208440104e2380e60b00ecee93a2a66bbc6c3
iwell92 <iwell92@live.com>
2021-10-07 19:42:39 -0300

    Commit para teste do projeto.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send({message: 'Aplicação online'});
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
